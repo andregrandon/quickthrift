d5dbf91e1a629626b12d721c3b79bc41bc5ea63b
andregrandon <gran1360@mylaurier.ca>
2022-02-18 18:39:30 -0500

    fix

--- mio/settings.py
+++ mio/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from dotenv import load_dotenv
 load_dotenv()
 import os
+import dj_database_url
 from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
